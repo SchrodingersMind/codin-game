d94874403f7c9f23a4f47a16b578fd82664ae540
SchrodingersMind <vzhytnyk31415@gmail.com>
2022-01-18 23:15:22 +0200

    Template solution for the winamax sponsored contest (but better ignore this)

--- cg.cpp
+++ cg.cpp
@@ -5,117 +5,138 @@
 
 using namespace std;
 
-class Node {
-public:
-    Node(): name(-1), estimated(0), f_value(0) {};
-    Node(int name, int estimated, int f_value): name(name), estimated(estimated), f_value(f_value) {};
-
-    int name;
-    int estimated;
-    int f_value;
-    int real_dist = 0;
-    bool fired = false;
-    bool in_queue = false;
+enum Directions {
+    kNone,
+    kRight,
+    kDown,
+    kLeft,
+    kUp
 };
 
-std::vector<std::vector<int>> map;
-std::vector<Node> nodes;
 
-Node aStarGetNextNode(std::vector<Node>& nodes_queue) {
-    Node min_node = nodes_queue[0];
-    int min_position = 0;
-    for (int i=1; i<nodes_queue.size(); i++) {
-        Node cur_node = nodes_queue[i];
-        if ((cur_node.f_value == min_node.f_value && cur_node.name < min_node.name) || min_node.f_value > cur_node.f_value) {
-            min_node = cur_node;
-            min_position = i;
-        }
+//////// Ball class
+
+class Ball {
+public:
+    int x, y;
+    int shot_count;
+    int last_shot_count;
+    vector<Directions> directions;
+
+    Ball(int x, int y, int shot_count): x(x), y(y), shot_count(shot_count), last_shot_count(shot_count) {};
+
+    void undo();
+    bool is_movable() const;
+    void move(Directions dir);
+};
+
+void Ball::undo() {
+    if (shot_count == last_shot_count) {
+        cout << "There is going something fishy -_-" << endl;
     }
-    nodes_queue.erase(nodes_queue.begin()+min_position);
-    return min_node;
+    last_shot_count++;
+    directions.pop_back();
+}
+void Ball::move(Directions dir) {
+    last_shot_count--;
+    directions.push_back(dir);
+}
+bool Ball::is_movable() const {
+    return last_shot_count != 0;
 }
 
-std::vector<int> getSiblings(int elem) {
-    std::vector<int> result;
-    for (int i=0; i<map.size(); i++) {
-        if (map[elem][i])
-            result.push_back(i);
-    }
-    return result;
+bool compareBalls (Ball ball1, Ball ball2) {
+    return ball1.shot_count < ball2.shot_count;
 }
-std::vector<Node> aStarAlg (int start_elem, int end_elem) {
-    std::vector<Node> result;
-    std::vector<Node> nodes_queue;
-    nodes[start_elem].fired = true;
-    nodes_queue.push_back(nodes[start_elem]);
-    bool done = false;
-    while (! done) {
-        Node cur_node = aStarGetNextNode(nodes_queue);
-        result.push_back(cur_node);
-        cur_node.fired = true;
-        if (cur_node.name == end_elem) {
-            done = true;
-            break;
-        }
-        std::vector<int> siblings = getSiblings(cur_node.name);
-        for (int sib_elem : siblings) {
-            Node sib_node = nodes[sib_elem];
-            if (sib_node.fired)
-                continue;
+//////// End of the Ball class
 
-            sib_node.real_dist = cur_node.real_dist + map[cur_node.name][sib_node.name];
-            sib_node.f_value = sib_node.estimated + sib_node.real_dist;
 
-            if (sib_node.in_queue) {
-                for (Node& tmp_node : nodes_queue) {
-                    if (tmp_node.name == sib_node.name && tmp_node.real_dist > sib_node.real_dist) {
-                        tmp_node.real_dist = sib_node.real_dist;
-                        tmp_node.f_value = sib_node.f_value;
-                        nodes[sib_elem] = sib_node;
-                    }
-                }
-            } else {
-                sib_node.in_queue = true;
-                nodes[sib_elem] = sib_node;
-                nodes_queue.push_back(sib_node);
+
+//////// Map class
+enum MapElems {
+    kLand,
+    kBall,
+    kPool,
+    kHole
+};
+enum OutMapElems {
+    kDot,
+    kArrow_right,
+    kArrow_down,
+    kArrow_left,
+    kArrow_up,
+    kHole_empty,
+    kHole_filled
+};
+
+class Map {
+private:
+    void makeMove();
+    void sortBalls();
+public:
+    int width;
+    int height;
+    vector<vector<int>> actual_map;
+    vector<vector<int>> result_map;
+    vector<Ball> balls;
+
+    Map(int width, int height): width(width), height(height) {};
+
+    void input();
+    void playGame();
+};
+
+void Map::makeMove() {
+
+}
+
+void Map::sortBalls() {
+
+}
+
+void Map::input() {
+    for (int l = 0; l < height; l++) {
+        string row;
+        cin >> row; cin.ignore();
+        actual_map.emplace_back(0);
+        result_map.emplace_back(0);
+        for (int k=0; k < row.size(); k++) {
+            char c = row[k];
+            switch (c) {
+                case 'X':
+                    actual_map[l].push_back(MapElems::kPool);
+                    result_map[l].push_back(OutMapElems::kDot);
+                    break;
+                case '.':
+                    actual_map[l].push_back(MapElems::kLand);
+                    result_map[l].push_back(OutMapElems::kDot);
+                    break;
+                case 'H':
+                    actual_map[l].push_back(MapElems::kHole);
+                    result_map[l].push_back(OutMapElems::kHole_empty);
+                default:
+                    // Probably it's number from 1 to 9, that assume bat shot_count
+                    Ball ball = Ball(l, k, c-'0');
+                    balls.push_back(ball);
+                    actual_map[l].push_back(MapElems::kBall);
+                    result_map[l].push_back(OutMapElems::kDot);
+                    break;
+
             }
         }
     }
-    return result;
+    // Now balls with lower shot count should go first
+    sort(balls.begin(), balls.end(), compareBalls);
 }
+//////// End of the Map class
 
 int main()
 {
-    int n;
-    int e;
-    int start;
-    int goal;
-    cin >> n >> e >> start >> goal; cin.ignore();
-//    nodes.resize(n);
-    map.resize(n);
-
-    for (int i = 0; i < n; i++) {
-        int heuristic_to_end;
-        cin >> heuristic_to_end; cin.ignore();
-        map[i].resize(n, 0);
-        nodes.emplace_back(i, heuristic_to_end, heuristic_to_end);
+    int width;
+    int height;
+    cin >> width >> height; cin.ignore();
+    for (int i = 0; i < height; i++) {
+        string row;
+        cin >> row; cin.ignore();
     }
-//    for (Node node : nodes) {
-//        cout << node.name << " " << node.f_value << endl;
-//    }
-
-    for (int i = 0; i < e; i++) {
-        int x;
-        int y;
-        int c;
-        cin >> x >> y >> c; cin.ignore();
-        map[x][y] = c;
-        map[y][x] = c;
-    }
-    std::vector<Node> a_star_result = aStarAlg(start, goal);
-
-    for (Node node : a_star_result) {
-        cout << node.name << " " << node.f_value << endl;
-    }
-//    cout << "0 0" << endl;
 }
