5305896a56dbddcc8778096f8f235e9783cbb91b
SchrodingersMind <vzhytnyk31415@gmail.com>
2022-01-20 20:28:04 +0200

    Solved CodeGames challenge - Brain Fork (game for the optimisation, maybe improve lately? currently 934-th position in leaderboard)

--- cg.cpp
+++ cg.cpp
@@ -5,87 +5,114 @@
 
 using namespace std;
 
-typedef unsigned long long ull;
-typedef unsigned long ul;
+char abc[] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
 
-ull getEarning(ul* group, int group_size, ul seats, int& iterator) {
-//    int group_size = group.size();
-    int start_iterator = (iterator == 0) ? group_size-1 : iterator-1;
-    ull earns=0;
-    if (group_size == 1)
-        return group[0];
-
-    while (start_iterator!=iterator && seats>earns) {
-        iterator++;
-        iterator = iterator%group_size;
-        earns += group[iterator];
-    }
-    if (earns > seats) {
-        earns -= group[iterator];
-        iterator--;
-    } else if (earns + group[(iterator+1)%group_size] < seats) {
-        iterator++;
-        earns += group[iterator%group_size];
-    }
-    return earns;
+int getNumber(char ch) {
+    if (ch == ' ')
+        return 0;
+    return ch - 'A' + 1;
 }
+
+int getDifference(char start, char end) {
+    int start_p = getNumber(start), end_p = getNumber(end);
+    int diff = max(start_p, end_p) - min(start_p, end_p);
+    if (diff > 13) {
+        diff = 27-diff;
+        if (start_p < end_p) {
+            diff = -diff;
+        }
+    } else if (start_p > end_p) {
+        diff = -diff;
+    }
+    return diff;
+}
+
+class Game {
+private:
+    string findBestOneWay(char ch);
+    vector<int> getNeighbors();
+    void moveLeft(int repeat);
+    void moveRight(int repeat);
+public:
+    vector<char> runes = vector<char>(30, ' ');
+    int blub_pos = 15;
+
+    string findWays(const string& inp_str);
+};
+
+void Game::moveLeft(int repeat) {
+    blub_pos -= repeat;
+    if (blub_pos < 0) {
+        blub_pos = 30 + blub_pos;
+    }
+}
+void Game::moveRight(int repeat) {
+    blub_pos += repeat;
+    if (blub_pos > 30) {
+        blub_pos = blub_pos - 30;
+    }
+}
+vector<int> Game::getNeighbors() {
+    vector<int> result;
+//    if (blub_pos == 0) {
+//        return {30-1, 0, 1};
+//    } else if (blub_pos == 30-1) {
+//        return {30-2, 30-1, 0};
+//    }
+//    return {blub_pos-1, blub_pos, blub_pos+1};
+    for (int i=blub_pos-14; i<blub_pos+15;i++) {
+        result.push_back(i%30);
+    }
+    return result;
+}
+
+string Game::findBestOneWay(char ch) {
+    string result;
+    vector<int> neighbors = getNeighbors();
+    int min_value = 100;
+    int min_index = 0;
+    for (int i=0; i<neighbors.size(); i++) {
+        int tmp_diff = abs(getDifference(runes[neighbors[i]], ch)) + abs((int)neighbors.size()/2 - i);
+        if (tmp_diff < min_value) {
+            min_value = tmp_diff;
+            min_index = i;
+        }
+    }
+    cerr << "Found nearest move to the " << neighbors[min_index] << " and all moves " << min_value << endl;
+    // What to do: move character to the neighbor, rotate rune and spell letter
+    if (min_index < (int)neighbors.size()/2) {
+        result.resize(result.size()+neighbors.size()/2-min_index, '<');
+        moveLeft((int)neighbors.size()/2-min_index);
+    } else {
+        result.resize(result.size()+min_index-neighbors.size()/2, '>');
+        moveRight((int)min_index-neighbors.size()/2);
+    }
+    int rune_index = neighbors[min_index];
+    int diff = getDifference(runes[rune_index], ch);
+    if (diff > 0) {
+        result.resize(result.size()+diff, '+');
+    } else {
+        result.resize(result.size()-diff, '-');
+    }
+    runes[rune_index] = ch;
+    result.append(".");
+    return result;
+}
+
+string Game::findWays(const string& inp_str) {
+    string result;
+    for (char ch : inp_str) {
+        result += findBestOneWay(ch);
+    }
+    return result;
+}
+
 int main()
 {
-    ull earned=0;
-    ul seats;
-    ul c;
-    int group_size;
-    cin >> seats >> c >> group_size; cin.ignore();
-    ul *groups_queue = new ul[group_size];
-    ul *race_earns = new ul[group_size];
-    int *iterator_shift = new int[group_size];
-    for (int i = 0; i < group_size; i++) {
-        int pi;
-        cin >> pi; cin.ignore();
-        groups_queue[i] = pi;
-        race_earns[i] = 0;
-        iterator_shift[i] = 0;
-    }
+    Game game;
+    string magic_phrase;
+    getline(cin, magic_phrase);
+    cerr << "Secret message is: " << magic_phrase << endl;
 
-    int iterator = group_size - 1;
-    if (group_size == 1) {
-//        earns += group[0];
-        cout << groups_queue[0]*c << endl;
-        return 0;
-    }
-
-    for (ul i=0; i<c; i++) {
-        ul tmp_earned=0;
-//        tmp_earned += getEarning(groups_queue, group_size, seats, iterator);
-        int start_iterator = iterator; //(iterator == 0) ? group_size-1 : iterator-1;
-//        ull tmp_earned=0;
-
-        if (race_earns[start_iterator]) {
-            earned += race_earns[start_iterator];
-            iterator = iterator_shift[start_iterator];
-            continue;
-        }
-
-        do {
-            iterator++;
-            iterator = iterator%group_size;
-            tmp_earned += groups_queue[iterator];
-        } while (start_iterator!=iterator && seats > tmp_earned);
-        if (tmp_earned > seats) {
-            tmp_earned -= groups_queue[iterator];
-            iterator--;
-            if (iterator < 0) {
-                iterator = group_size-1;
-                cerr << "Wrong output hereeee!" << endl;
-            }
-            // } else if (tmp_earned + groups_queue[(iterator + 1) % group_size] < seats) {
-            //     iterator++;
-            //     tmp_earned += groups_queue[iterator % group_size];
-        }
-        race_earns[start_iterator] = tmp_earned;
-        iterator_shift[start_iterator] = iterator;
-        earned += tmp_earned;
-    }
-
-    cout << earned << endl;
+    cout << game.findWays(magic_phrase) << endl;
 }
