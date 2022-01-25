8be19a45094607d556f06bb8500c4b216b122d59
SchrodingersMind <vzhytnyk31415@gmail.com>
2022-01-25 20:56:01 +0200

    Solved CodeGames challenge - Code4Life (game for the bot construction, maybe try to improve it sometimes...)

--- cg.cpp
+++ cg.cpp
@@ -1,118 +1,255 @@
 #include <iostream>
 #include <string>
+#include <utility>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-char abc[] = {' ', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+enum PlayerID_e {
+    kNone = -1,
+    kMe = 0,
+    kOpponent = 1
+};
 
-int getNumber(char ch) {
-    if (ch == ' ')
-        return 0;
-    return ch - 'A' + 1;
-}
+class Vitamin{
+public:
+    Vitamin(int v_a, int v_b, int v_c, int v_d, int v_e);
 
-int getDifference(char start, char end) {
-    int start_p = getNumber(start), end_p = getNumber(end);
-    int diff = max(start_p, end_p) - min(start_p, end_p);
-    if (diff > 13) {
-        diff = 27-diff;
-        if (start_p < end_p) {
-            diff = -diff;
-        }
-    } else if (start_p > end_p) {
-        diff = -diff;
+    int a, b, c, d, e;
+    Vitamin(Vitamin const &copied) {
+        a = copied.a;
+        b = copied.b;
+        c = copied.c;
+        d = copied.d;
+        e = copied.e;
     }
-    return diff;
+    string getNeeded(const Vitamin& fulfill) const;
+    int summary() const;
+};
+
+string Vitamin::getNeeded(const Vitamin& fulfill) const {
+    if (a < fulfill.a)
+        return "A";
+    if (b < fulfill.b)
+        return "B";
+    if (c < fulfill.c)
+        return "C";
+    if (d < fulfill.d)
+        return "D";
+    if (e < fulfill.e)
+        return "E";
+    return "";
 }
+
+int Vitamin::summary() const {
+    return a+b+c+d+e;
+}
+
+Vitamin::Vitamin(int v_a, int v_b, int v_c, int v_d, int v_e) {
+    a = v_a;
+    b = v_b;
+    c = v_c;
+    d = v_d;
+    e = v_e;
+}
+
+class Sample {
+public:
+    int sample_id=-1;
+    PlayerID_e carried_by=kNone;
+//    int rank;
+//    string expertise_gain;
+    int health{};
+    Vitamin vitamin;
+//    int cost_a{};
+//    int cost_b{};
+//    int cost_c{};
+//    int cost_d{};
+//    int cost_e{};
+
+    double getCostBenefitRatio() const;
+    bool isValid() const;
+};
+
+double Sample::getCostBenefitRatio() const {
+    return (double)health / vitamin.summary();
+}
+
+bool Sample::isValid() const {
+    return sample_id != -1;
+}
+
+// sort samples in descending order
+bool sortSamples(const Sample& sample1, const Sample& sample2) {
+    return sample1.getCostBenefitRatio() > sample2.getCostBenefitRatio();
+}
+
+
+class Robot {
+//    PlayerID_e owned_by;
+    string target;
+//    int eta;
+    int score;
+public:
+    Vitamin carried_vitamins;
+//    int expertise_e;
+//    int expertise_d;
+//    int expertise_c;
+//    int expertise_b;
+//    int expertise_a;
+    Sample *cur_sample = nullptr; //Sample();
+    Robot(string target, int score, const Vitamin& vitamin):
+            target{std::move(target)}, score{score}, carried_vitamins{vitamin} {}
+
+
+    void update(Robot* copy_robot);
+    string getTarget() { return target; }
+    void setTarget(string new_target){ target = std::move(new_target); };
+    string neededMolecules();
+    bool isCarriedSample() const;
+};
+
+void Robot::update(Robot* copy_robot) {
+    target = copy_robot->getTarget();
+    score = copy_robot->score;
+    carried_vitamins = copy_robot->carried_vitamins;
+}
+
+string Robot::neededMolecules() {
+    return carried_vitamins.getNeeded(cur_sample->vitamin);
+}
+
+bool Robot::isCarriedSample() const {
+    return cur_sample != nullptr;
+}
+
 
 class Game {
 private:
-    string findBestOneWay(char ch);
-    vector<int> getNeighbors();
-    void moveLeft(int repeat);
-    void moveRight(int repeat);
-public:
-    vector<char> runes = vector<char>(30, ' ');
-    int blub_pos = 15;
+    bool is_init = true;
+    vector<Sample> useful_medicines;
+    vector<string> next_moves;
+    Robot *me, *enemy;
 
-    string findWays(const string& inp_str);
+    Robot* inputPlayer();
+public:
+    void inputTurn();
+    string getCurrentTurn();
 };
 
-void Game::moveLeft(int repeat) {
-    blub_pos -= repeat;
-    if (blub_pos < 0) {
-        blub_pos = 30 + blub_pos;
-    }
-}
-void Game::moveRight(int repeat) {
-    blub_pos += repeat;
-    if (blub_pos > 30) {
-        blub_pos = blub_pos - 30;
-    }
-}
-vector<int> Game::getNeighbors() {
-    vector<int> result;
-//    if (blub_pos == 0) {
-//        return {30-1, 0, 1};
-//    } else if (blub_pos == 30-1) {
-//        return {30-2, 30-1, 0};
-//    }
-//    return {blub_pos-1, blub_pos, blub_pos+1};
-    for (int i=blub_pos-14; i<blub_pos+15;i++) {
-        result.push_back(i%30);
-    }
-    return result;
+Robot* Game::inputPlayer() {
+    string target;
+    int eta;
+    int score;
+    int storage_a;
+    int storage_b;
+    int storage_c;
+    int storage_d;
+    int storage_e;
+    int expertise_a;
+    int expertise_b;
+    int expertise_c;
+    int expertise_d;
+    int expertise_e;
+    cin >> target >> eta >> score >> storage_a >> storage_b >> storage_c >> storage_d >> storage_e >> expertise_a >> expertise_b >> expertise_c >> expertise_d >> expertise_e; cin.ignore();
+    Vitamin vitamin {storage_a, storage_b, storage_c, storage_d, storage_e};
+    return new Robot{target, score, vitamin};
 }
 
-string Game::findBestOneWay(char ch) {
+void Game::inputTurn() {
+    if (is_init) {
+        me = inputPlayer();
+    } else {
+        // Hollow input
+        me->update(inputPlayer());
+    }
+    enemy = inputPlayer();
+    useful_medicines.clear();
+    // Ignore
+    int available_a;
+    int available_b;
+    int available_c;
+    int available_d;
+    int available_e;
+    cin >> available_a >> available_b >> available_c >> available_d >> available_e; cin.ignore();
+    int sample_count;
+    cin >> sample_count; cin.ignore();
+    for (int i = 0; i < sample_count; i++) {
+        int sample_id;
+        int carried_by;
+        int rank;
+        string expertise_gain;
+        int health;
+        int cost_a;
+        int cost_b;
+        int cost_c;
+        int cost_d;
+        int cost_e;
+        cin >> sample_id >> carried_by >> rank >> expertise_gain >> health >> cost_a >> cost_b >> cost_c >> cost_d >> cost_e; cin.ignore();
+        Vitamin vitamin {cost_a, cost_b, cost_c, cost_d, cost_e};
+        Sample sample{sample_id, (PlayerID_e)carried_by, health,vitamin};
+        if (carried_by == PlayerID_e::kNone) {
+            useful_medicines.push_back(sample);
+        }
+
+    }
+    sort(useful_medicines.begin(), useful_medicines.end(), sortSamples);
+    is_init = false;
+}
+
+string Game::getCurrentTurn() {
     string result;
-    vector<int> neighbors = getNeighbors();
-    int min_value = 100;
-    int min_index = 0;
-    for (int i=0; i<neighbors.size(); i++) {
-        int tmp_diff = abs(getDifference(runes[neighbors[i]], ch)) + abs((int)neighbors.size()/2 - i);
-        if (tmp_diff < min_value) {
-            min_value = tmp_diff;
-            min_index = i;
+    if (! next_moves.empty()) {
+        // Still got the moves
+        result = next_moves.front();
+        next_moves.erase(next_moves.begin());
+        return result;
+    }
+
+    if (! me->isCarriedSample()) {
+        if (me->getTarget() == "DIAGNOSIS") {
+            me->cur_sample =  new Sample{useful_medicines[0]};
+            return "CONNECT " + to_string(useful_medicines[0].sample_id);
+        } else {
+            me->setTarget("DIAGNOSIS");
+            return "GOTO DIAGNOSIS";
         }
     }
-    cerr << "Found nearest move to the " << neighbors[min_index] << " and all moves " << min_value << endl;
-    // What to do: move character to the neighbor, rotate rune and spell letter
-    if (min_index < (int)neighbors.size()/2) {
-        result.resize(result.size()+neighbors.size()/2-min_index, '<');
-        moveLeft((int)neighbors.size()/2-min_index);
-    } else {
-        result.resize(result.size()+min_index-neighbors.size()/2, '>');
-        moveRight((int)min_index-neighbors.size()/2);
+    // So, we have a sample and need take needed molecules or send it to the laboratory
+    string needed_molecules = me->neededMolecules();
+    if (needed_molecules.empty()) {
+        next_moves.push_back("CONNECT " + to_string(me->cur_sample->sample_id));
+        me->cur_sample = nullptr;
+        return "GOTO LABORATORY";
     }
-    int rune_index = neighbors[min_index];
-    int diff = getDifference(runes[rune_index], ch);
-    if (diff > 0) {
-        result.resize(result.size()+diff, '+');
+    if (me->getTarget() == "MOLECULES") {
+        return "CONNECT " + needed_molecules;
     } else {
-        result.resize(result.size()-diff, '-');
+        return "GOTO MOLECULES";
     }
-    runes[rune_index] = ch;
-    result.append(".");
-    return result;
 }
 
-string Game::findWays(const string& inp_str) {
-    string result;
-    for (char ch : inp_str) {
-        result += findBestOneWay(ch);
-    }
-    return result;
-}
 
 int main()
 {
-    Game game;
-    string magic_phrase;
-    getline(cin, magic_phrase);
-    cerr << "Secret message is: " << magic_phrase << endl;
+    // Ignore
+    int project_count;
+    cin >> project_count; cin.ignore();
+    for (int i = 0; i < project_count; i++) {
+        int a;
+        int b;
+        int c;
+        int d;
+        int e;
+        cin >> a >> b >> c >> d >> e; cin.ignore();
+    }
 
-    cout << game.findWays(magic_phrase) << endl;
+    Game game;
+    // game loop
+    while (1) {
+        game.inputTurn();
+
+        cout << game.getCurrentTurn() << endl;
+    }
 }
